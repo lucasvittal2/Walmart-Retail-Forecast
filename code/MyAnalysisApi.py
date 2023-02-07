2c9f5a97833f6a94d943bda988cb48c7497fdafc
lucasvittal2 <lucasvittal@gmail.com>
2023-02-07 17:21:54 -0300

    cleaning data   and eliminating outliers

---
+++ code/MyAnalysisApi.py
@@ -0,0 +1,5 @@
+import os
+from Parameters import THESHOLD_SALES_VALUE, PROJECT_ROOT_PATH
+
+def setup_env():
+    os.chdir(PROJECT_ROOT_PATH)
---
+++ code/Parameters.py
@@ -0,0 +1,3 @@
+
+THESHOLD_SALES_VALUE = 2e5,
+PROJECT_ROOT_PATH = "C:\\Users\\lucas\\Documents\\data_science projects\\Walmart Retail Forecast"
